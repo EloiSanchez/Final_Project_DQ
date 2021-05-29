9738aa6303e66b69e1c1fcf28046ba8ed5279ddd
Eloi Sanchez <eloisanchez16@gmail.com>
2021-05-29 10:42:22 +0200

    Fixed TISE solving and added eigenstate plots

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ numEigStates = 3 # Number of eigenstates to find
 L = 19. # Distance between the fixed ions
 
 # Electron grid
-elecDim = 201
-elecSpace = np.linspace(-L/2, L/2, elecDim)
+elecDim = 401
+elecSpace = np.linspace(-L, L, elecDim)
 elecDx = elecSpace[1] - elecSpace[0]
 elecEye = np.identity(elecDim)
 
@@ -43,8 +43,8 @@ nucEye = np.identity(nucDim)
 
 #####################################
 
-potLeft = op.softCoulombFixed(elecSpace, -L/2, 3.1)
-potRight = op.softCoulombFixed(elecSpace, L/2, 4.4)
+potLeft = op.softCoulombFixed(elecSpace, -L/2, 4.4)
+potRight = op.softCoulombFixed(elecSpace, L/2, 3.1)
 
 elecHam = op.kin(elecDim, 1, elecDx) - potLeft - potRight # + potential operators to be done
 
@@ -56,18 +56,22 @@ for i in range(nucDim):
     elecHamR.append(elecHam - op.softCoulombFixed(elecSpace, nucSpace[i], 7))
     elecEigenvalues[:,i], elecEigenstates[:,:,i] = eigh(elecHamR[i], subset_by_index=[0, numEigStates - 1])
 
-fig3 = plt.figure()
-ax3 = fig3.add_subplot()
+fig3 = plt.figure(figsize=(6.4*2, 4.8))
+ax3_vals = fig3.add_subplot(121)
+ax3_states = fig3.add_subplot(122)
 
 elecEigenvalues += 1 / np.abs(nucSpace - L/2) + 1 / np.abs(nucSpace + L/2)
 
 for i in range(numEigStates):
-    ax3.plot(nucSpace, elecEigenvalues[i,:], label="state {}".format(i))
+    ax3_vals.plot(nucSpace, elecEigenvalues[i,:], label="state {}".format(i))
+    ax3_states.plot(elecSpace, elecEigenstates[:,i,nucDim//2], label="state {}".format(i))
 
+ax3_vals.set_ylim((-0.25,0.1))
+ax3_vals.legend()
+ax3_vals.set_title("BOPES")
 
-# ax3.set_xlim((-8,8))
-ax3.set_ylim((-0.25,0.1))
-ax3.legend()
+ax3_states.legend()
+ax3_states.set_title(r"Eigenstates with $R={}$".format(nucSpace[nucDim//2]))
 
 #####################################
 # Per veure els eigenstates descomentar aquest codi
