799795528146401f876ca5590ecccdbd8705b11b
Eloi Sanchez <eloisanchez16@gmail.com>
2021-06-15 21:12:34 +0200

    Dynamics with second excited state

--- input.py
+++ input.py
@@ -28,7 +28,7 @@ sigma = 1. / (2.85)**(0.5)
 
 # Initial Electronic Eigenstate
 # = 0 (GS); = 1 (1st excited), = 2 (2nd excited), etc.
-initEigenstate = 1
+initEigenstate = 2
 
 # Directory name to save the results
-resultsDir = 'firstExcited'
+resultsDir = 'secondExcited'
